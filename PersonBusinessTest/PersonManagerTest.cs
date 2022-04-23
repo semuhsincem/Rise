b00a9c8958acef61914a0b433ad9ba1b693dc56e
semuhsincem <se.muhsincem@gmail.com>
2022-04-23 12:10:26 +0300

    ++

---
+++ PersonBusinessTest/PersonManagerTest.cs
@@ -0,0 +1,100 @@
+﻿using DeepEqual.Syntax;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using Rise.BusinessLayer.Abstract;
+using Rise.BusinessLayer.Concrete;
+using Rise.Entity.Concrete;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PersonBusinessTest
+{
+    public class PersonManagerTest
+    {
+        private readonly IPersonService _personService;
+        public PersonManagerTest(IPersonService personService)
+        {
+            _personService = personService;
+        }
+        [Fact]
+        public async Task Create_Person_Test()
+        {
+            var person = new Person()
+            {
+                Company = "Test Company",
+                Name = "Test User Name",
+                Surname = "Test User Last Name"
+            };
+            var result = await _personService.CreatePerson(person);
+            var testResult = (object)result.Data.IsDeepEqual(new Person()
+            {
+                Id = result.Data?.Id,
+                Company = "Test Company",
+                Name = "Test User Name",
+                Surname = "Test User Last Name"
+            });
+            Assert.True(Convert.ToBoolean(testResult));
+        }
+        [Fact]
+        public async Task Add_Info_To_Person_Test()
+        {
+            var person = new Person()
+            {
+                Company = "Update Person Test Company",
+                Name = "Update Person Test User Name",
+                Surname = "Update Person Test User Last Name"
+            };
+            var addedPerson = await _personService.CreatePerson(person);
+            if (addedPerson.IsSuccess)
+            {
+                //Update User
+                var addInfoUser = await _personService.AddInfo(new PersonDetails()
+                {
+                    ContactInfo = "11111",
+                    ContactType = EnumHelper.EContactType.Phone,
+                    PersonId = addedPerson.Data?.Id
+                });
+
+                if (addInfoUser.IsSuccess)
+                {
+                    var testResult = (object)addInfoUser.Data.IsDeepEqual(new PersonDetails()
+                    {
+                        Id = addInfoUser.Data?.Id,
+                        ContactInfo = "11111",
+                        ContactType = EnumHelper.EContactType.Phone,
+                        PersonId = addedPerson.Data?.Id
+                    });
+                    if (Convert.ToBoolean(testResult))
+                        Assert.True(Convert.ToBoolean(testResult));
+                    else
+                        Assert.True(false);
+
+                }
+                else
+                    Assert.True(false, "An error occured while add info to person...");
+
+            }
+            else
+                Assert.True(false, "An error occured while add person...");
+        }
+        [Fact]
+        public async Task Remove_Person_Test()
+        {
+            var person = new Person()
+            {
+                Company = "Remove Person Test Company",
+                Name = "Remove Person Test User Name",
+                Surname = "Remove Person Test User Last Name"
+            };
+            var addedPerson = await _personService.CreatePerson(person);
+            if (addedPerson.IsSuccess)
+            {
+                var removePerson = await _personService.RemovePerson(person.Id);
+                Assert.True(removePerson.IsSuccess);
+            }
+            else
+                Assert.True(false, "An error occured while add person...");
+        }
+    }
+}
---
+++ Rise.xUnitTests/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rise.BusinessLayer.Abstract;
+using Rise.BusinessLayer.Concrete;
+using Rise.DAL;
+using Rise.DAL.Abstract;
+using Rise.DAL.Concrete;
+using Rise.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rise.Tests
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<MongoDbSettings>(options =>
+            {
+                options.ConnectionString = "mongodb://localhost:27017";
+                options.Database = "RiseDbTest";
+            });
+            services.AddScoped<IPersonService, PersonManager>();
+            services.AddScoped<IPersonDal, PersonMongoDbDal>();
+            services.AddScoped<IPersonDetailsDal, PersonDetailsMongoDbDal>();
+            services.AddScoped<IReportService, ReportManager>();
+            services.AddScoped<IReportDal, ReportMongoDbDal>();
+        }
+    }
+}
